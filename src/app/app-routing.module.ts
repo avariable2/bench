d97d218c5305aa5e678358fb0ef242b42bf38ba3
didilero <didileroVA1999@hotmail.fr>
2021-02-08 18:02:20 +0100

    Oui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { IndexComponent } from './index/index.component';
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
+  { path: 'bench', component: IndexComponent },
 ];
 
 @NgModule({
