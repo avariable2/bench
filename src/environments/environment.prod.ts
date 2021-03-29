992d1684c3b3c316828373f0d50af4367ef44ff4
didilero <didileroVA1999@hotmail.fr>
2021-03-30 01:10:27 +0200

    Modif pour build demo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,13 @@
 export const environment = {
-  production: true
-};
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyBOY8f5tl--DXtEnkGZF-UtVmhFm7eUKPs",
-  authDomain: "site-ben-d41dc.firebaseapp.com",
-  databaseURL: "https://site-ben-d41dc.firebaseio.com",
-  projectId: "site-ben-d41dc",
-  storageBucket: "site-ben-d41dc.appspot.com",
-  messagingSenderId: "311740316689",
-  appId: "1:311740316689:web:6f285be0aab753740b45b0",
-  measurementId: "G-LCGJ6HT2YB"
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBOY8f5tl--DXtEnkGZF-UtVmhFm7eUKPs',
+    authDomain: 'site-ben-d41dc.firebaseapp.com',
+    databaseURL: 'https://site-ben-d41dc.firebaseio.com',
+    projectId: 'site-ben-d41dc',
+    storageBucket: 'site-ben-d41dc.appspot.com',
+    messagingSenderId: '311740316689',
+    appId: '1:311740316689:web:6f285be0aab753740b45b0',
+    measurementId: 'G-LCGJ6HT2YB',
+  },
 };
